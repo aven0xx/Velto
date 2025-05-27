36d79f99ab7a7b80ea933415db73261d86a8ca79
BuildTools <unconfigured@null.spigotmc.org>
2025-05-27 17:49:33 -0400

    Commands.yml is now supposed to work correctly. And also added previously added command in commands.yml.

--- src/main/java/com/aven0x/xcore/manager/CommandManager.java
+++ src/main/java/com/aven0x/xcore/manager/CommandManager.java
@@ -1,26 +1,27 @@
 package com.aven0x.xcore.manager;
 
 import com.aven0x.xcore.commands.*;
+import com.aven0x.xcore.utils.CommandUtil;
 
 public class CommandManager {
     public static void registerAllCommands() {
-        new SpawnCommand();
-        new SetSpawnCommand();
-        new TimeCommand();
-        new DayCommand();
-        new NightCommand();
-        new CraftCommand();
-        new AnvilCommand();
-        new GamemodeCommand();
-        new GamemodeSurvivalCommand();
-        new GamemodeCreativeCommand();
-        new GamemodeAdventureCommand();
-        new GamemodeSpectatorCommand();
-        new FeedCommand();
-        new HealCommand();
-        new BroadcastCommand();
-        new WeatherCommand();
-        new KillCommand();
-        new SpeedCommand();
+        if (CommandUtil.isEnabled("spawn")) new SpawnCommand();
+        if (CommandUtil.isEnabled("setspawn")) new SetSpawnCommand();
+        if (CommandUtil.isEnabled("time")) new TimeCommand();
+        if (CommandUtil.isEnabled("day")) new DayCommand();
+        if (CommandUtil.isEnabled("night")) new NightCommand();
+        if (CommandUtil.isEnabled("craft")) new CraftCommand();
+        if (CommandUtil.isEnabled("anvil")) new AnvilCommand();
+        if (CommandUtil.isEnabled("gamemode")) new GamemodeCommand();
+        if (CommandUtil.isEnabled("gms")) new GamemodeSurvivalCommand();
+        if (CommandUtil.isEnabled("gmc")) new GamemodeCreativeCommand();
+        if (CommandUtil.isEnabled("gma")) new GamemodeAdventureCommand();
+        if (CommandUtil.isEnabled("gmsp")) new GamemodeSpectatorCommand();
+        if (CommandUtil.isEnabled("feed")) new FeedCommand();
+        if (CommandUtil.isEnabled("heal")) new HealCommand();
+        if (CommandUtil.isEnabled("broadcast")) new BroadcastCommand();
+        if (CommandUtil.isEnabled("weather")) new WeatherCommand();
+        if (CommandUtil.isEnabled("kill")) new KillCommand();
+        if (CommandUtil.isEnabled("speed")) new SpeedCommand();
     }
 }
---
+++ src/main/java/com/aven0x/xcore/utils/CommandUtil.java
@@ -0,0 +1,24 @@
+package com.aven0x.xcore.utils;
+
+import com.aven0x.xcore.Xcore;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
+public class CommandUtil {
+    private static FileConfiguration commands;
+
+    public static void load() {
+        File file = new File(Xcore.getInstance().getDataFolder(), "commands.yml");
+        if (!file.exists()) {
+            Xcore.getInstance().saveResource("commands.yml", false);
+        }
+        commands = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public static boolean isEnabled(String commandName) {
+        if (commands == null) load();
+        return commands.getBoolean(commandName + ".enabled", true);
+    }
+}
